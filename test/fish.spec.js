b39431c90df7be54f6adf6f092c2ff5a114c5287
Johnny Reina <johnny.r.reina@gmail.com>
2018-07-02 22:53:10 -0500

    Changes unit test code to reflect what would be common usage.

--- test/fish.spec.js
+++ test/fish.spec.js
@@ -2,13 +2,18 @@ const { expect } = require('chai');
 const { compose, pipe } = require('../index');
 
 describe('kleisli', function() {
+  const composeP = compose('then');
+  const pipeP = pipe('then');
+  const e = x => x * 2;
+  const f = x => Promise.resolve(x + 1);
+  const g = x => Promise.resolve(x * 2);
+  const w = x => Promise.resolve(x * x);
   describe('#compose', function() {
-    const e = x => x * 2;
-    const f = x => Promise.resolve(x + 1);
-    const g = x => Promise.resolve(x * 2);
-    const w = x => Promise.resolve(x * x);
     it('Should compose two promise-returning functions', async function() {
-      const h = compose('then')(f, g);
+      const h = composeP(
+        f,
+        g
+      );
       const actual = await h(10);
       const expected = 21;
 
@@ -16,8 +21,20 @@ describe('kleisli', function() {
     });
 
     it('Should be associative with promise-returning functions.\n\t\t(g <=< f) <=< w = g <=< (f <=< w)', async function() {
-      const a = compose('then')(compose('then')(g, f), w);
-      const b = compose('then')(g, compose('then')(f, w));
+      const a = composeP(
+        composeP(
+          g,
+          f
+        ),
+        w
+      );
+      const b = composeP(
+        g,
+        composeP(
+          f,
+          w
+        )
+      );
       const actual = await a(10);
       const expected = await b(10);
 
@@ -25,7 +42,10 @@ describe('kleisli', function() {
     });
 
     it('Should compose one promise-returning function with one primitive-returning function', async function() {
-      const h = compose('then')(f, e);
+      const h = composeP(
+        f,
+        e
+      );
       const actual = await h(10);
       const expected = 21;
 
@@ -33,8 +53,20 @@ describe('kleisli', function() {
     });
 
     it('Should be associative with a mix of promise- and primitive-returning functions.\n\t\t(g <=< f) <=< w = g <=< (f <=< w)', async function() {
-      const a = compose('then')(compose('then')(g, f), e);
-      const b = compose('then')(g, compose('then')(f, e));
+      const a = composeP(
+        composeP(
+          g,
+          f
+        ),
+        e
+      );
+      const b = composeP(
+        g,
+        composeP(
+          f,
+          e
+        )
+      );
       const actual = await a(10);
       const expected = await b(10);
 
@@ -43,12 +75,11 @@ describe('kleisli', function() {
   });
 
   describe('#pipe', function() {
-    const e = x => x * 2;
-    const f = x => Promise.resolve(x + 1);
-    const g = x => Promise.resolve(x * 2);
-    const w = x => Promise.resolve(x * x);
     it('Should pipe two promise-returning functions', async function() {
-      const h = pipe('then')(f, g);
+      const h = pipeP(
+        f,
+        g
+      );
       const actual = await h(10);
       const expected = 22;
 
@@ -56,8 +87,20 @@ describe('kleisli', function() {
     });
 
     it('Should be associative with promise-returning functions.\n\t\t(g >=> f) >=> e = g >=> (f >=> e)', async function() {
-      const a = pipe('then')(pipe('then')(g, f), w);
-      const b = pipe('then')(g, pipe('then')(f, w));
+      const a = pipeP(
+        pipeP(
+          g,
+          f
+        ),
+        w
+      );
+      const b = pipeP(
+        g,
+        pipeP(
+          f,
+          w
+        )
+      );
       const actual = await a(10);
       const expected = await b(10);
 
@@ -65,7 +108,10 @@ describe('kleisli', function() {
     });
 
     it('Should pipe one promise-returning function with one primitive-returning function', async function() {
-      const h = pipe('then')(f, e);
+      const h = pipeP(
+        f,
+        e
+      );
       const actual = await h(10);
       const expected = 22;
 
@@ -73,8 +119,20 @@ describe('kleisli', function() {
     });
 
     it('Should be associative with a mix of promise- and primitive-returning functions.\n\t\t(g >=> f) >=> e = g >=> (f >=> e)', async function() {
-      const a = pipe('then')(pipe('then')(g, f), e);
-      const b = pipe('then')(g, pipe('then')(f, e));
+      const a = pipeP(
+        pipeP(
+          g,
+          f
+        ),
+        e
+      );
+      const b = pipeP(
+        g,
+        pipeP(
+          f,
+          e
+        )
+      );
       const actual = await a(10);
       const expected = await b(10);
 
